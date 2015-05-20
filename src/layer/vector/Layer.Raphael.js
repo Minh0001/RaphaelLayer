5ab203f07b96f09f716244d1097240f9dbce2789
Mih <quangminh0001@gmail.com>
2015-05-20 11:19:48 +0700

    fix _updateRaphaelViewport to work with leaflet 0.7.3

--- src/layer/vector/Layer.Raphael.js
+++ src/layer/vector/Layer.Raphael.js
@@ -1,82 +1,85 @@
 R.Layer = L.Class.extend({
-	includes: L.Mixin.Events,
-	
-	initialize: function(options) {
-		
-	},
+  includes: L.Mixin.Events,
 
-	onAdd: function (map) {
-		this._map = map;
-		this._map._initRaphaelRoot();
-		this._paper = this._map._paper;
-		this._set = this._paper.set();
-		
-		map.on('viewreset', this.projectLatLngs, this);
-		this.projectLatLngs();
-	},
+  initialize: function(options) {
 
-	onRemove: function(map) {
-		map.off('viewreset', this.projectLatLngs, this);
-		this._map = null;
-		this._set.forEach(function(item) {
-			item.remove();
-		}, this);
-		this._set.clear();
-	},
+  },
 
-	projectLatLngs: function() {
-		
-	},
+  onAdd: function(map) {
+    this._map = map;
+    this._map._initRaphaelRoot();
+    this._paper = this._map._paper;
+    this._set = this._paper.set();
 
-	animate: function(attr, ms, easing, callback) {
-		this._set.animate(attr, ms, easing, callback);
-	
-		return this;
-	},
+    map.on('viewreset', this.projectLatLngs, this);
+    this.projectLatLngs();
+  },
 
-	hover: function(f_in, f_out, icontext, ocontext) {
-		this._set.hover(f_in, f_out, icontext, ocontext);
+  onRemove: function(map) {
+    map.off('viewreset', this.projectLatLngs, this);
+    this._map = null;
+    this._set.forEach(function(item) {
+      item.remove();
+    }, this);
+    this._set.clear();
+  },
 
-		return this;
-	},
+  projectLatLngs: function() {
 
-	attr: function(name, value) {
-		this._set.attr(name, value);
+  },
 
-		return this;
-	}
+  animate: function(attr, ms, easing, callback) {
+    this._set.animate(attr, ms, easing, callback);
+
+    return this;
+  },
+
+  hover: function(f_in, f_out, icontext, ocontext) {
+    this._set.hover(f_in, f_out, icontext, ocontext);
+
+    return this;
+  },
+
+  attr: function(name, value) {
+    this._set.attr(name, value);
+
+    return this;
+  }
 });
 
 L.Map.include({
-	_initRaphaelRoot: function () {
-		if (!this._raphaelRoot) {
-			this._raphaelRoot = this._panes.overlayPane;
-			this._paper = Raphael(this._raphaelRoot);
+  _initRaphaelRoot: function() {
+    if (!this._raphaelRoot) {
+      this._raphaelRoot = L.DomUtil.create('div', 'rapahel-layer');
+      this._paper = Raphael(this._raphaelRoot);
 
-			this.on('moveend', this._updateRaphaelViewport);
-			this._updateRaphaelViewport();
-		}
-	},
+      var animated = false;
+      L.DomUtil.addClass(this._raphaelRoot, 'leaflet-zoom-' + (animated ?
+        'animated' : 'hide'));
 
-	_updateRaphaelViewport: function () {
-		var	p = 0.02,
-			size = this.getSize(),
-			panePos = L.DomUtil.getPosition(this._mapPane),
-			min = panePos.multiplyBy(-1)._subtract(size.multiplyBy(p)),
-			max = min.add(size.multiplyBy(1 + p*2)),
-			width = max.x - min.x,
-			height = max.y - min.y,
-			root = this._raphaelRoot,
-			pane = this._panes.overlayPane;
+      this._panes.overlayPane.appendChild(this._raphaelRoot);
 
-		this._paper.setSize(width, height);
-		
-		L.DomUtil.setPosition(root, min);
+      this.on('moveend', this._updateRaphaelViewport);
 
-		root.setAttribute('width', width);
-		root.setAttribute('height', height);
-		
-		this._paper.setViewBox(min.x, min.y, width, height, false);
-		
-	}
+      this._updateRaphaelViewport();
+    }
+  },
+
+  _updateRaphaelViewport: function() {
+    var p = 0.00,
+      size = this.getSize(),
+      panePos = L.DomUtil.getPosition(this._mapPane),
+      min = panePos.multiplyBy(-1)._subtract(size.multiplyBy(p)),
+      max = min.add(size.multiplyBy(1 + p * 2)),
+      width = max.x - min.x,
+      height = max.y - min.y,
+      root = this._raphaelRoot;
+
+    console.log('reset raphael', size, panePos, min, max, width, height);
+
+    this._paper.setSize(width, height);
+    L.DomUtil.setPosition(root, min);
+    this._paper.setViewBox(min.x, min.y, width, height, false);
+
+  }
 });
